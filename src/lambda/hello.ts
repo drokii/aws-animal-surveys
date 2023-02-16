75270b7a7779c9e5f73539d56976dc1b8a3b4033
Pedro Marques <pedrodossantosmarques@gmail.com>
2023-02-16 17:22:13 +0100

    Edited structure of project and managed to get terraform to work. Still need to figure out working lambda

--- src/lambda/hello.ts
+++ src/lambda/hello.ts
@@ -1,5 +1,4 @@
 import { APIGatewayEvent, APIGatewayProxyCallback, APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
-import { Duck } from '../entities/Animal'
 
 
 export const lambdaHandler = (event: APIGatewayEvent, context: Context, callback: APIGatewayProxyCallback): void => {
